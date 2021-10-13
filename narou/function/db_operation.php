5934437bb962ad2b61594b36160fe630b46061b3
kokimatsuno <65966275+kokimatsuno@users.noreply.github.com>
2021-10-13 17:54:49 +0900

    update

--- narou/function/db_operation.php
+++ narou/function/db_operation.php
@@ -144,17 +144,10 @@ function search_similar($pdo){
 
   //類似検索
   //対象similarity前後60件を抜き出し、一時テーブルへ保存
-  //$stmt = 
   $pdo->query("create temporary table {$tmp_table_name} as (select ncode,  similarity from {$table_name} where similarity < {$similarity} limit 60) union (select ncode,  similarity from {$table_name} where similarity > {$similarity} limit 60) order by abs(similarity - {$similarity}) limit 10 offset {$offset_par}");
-  //$stmt->execute();
 
-  //$stmt = 
   $pdo->query("alter table {$tmp_table_name} add id int not null primary key auto_increment first");
-  //$stmt->execute();
   
-  
-  //$stmt = $pdo->query("select {$tmp_table_name}.ncode, Naro_All_info.writer, Naro_All_info.title, Naro_All_info.story from {$tmp_table_name} inner join Naro_All_info on {$tmp_table_name}.ncode = Naro_All_info.ncode order by {$tmp_table_name}.id limit 10 offset {$offset_par}");
-  //$stmt->execute(); 
   $stmt = $pdo->query("select {$tmp_table_name}.ncode, Naro_All_info.writer, Naro_All_info.title, Naro_All_info.story from {$tmp_table_name} inner join Naro_All_info on {$tmp_table_name}.ncode = Naro_All_info.ncode order by {$tmp_table_name}.id");
   $result = $stmt->fetchAll();
 
@@ -163,19 +156,4 @@ function search_similar($pdo){
   $pdo = null;
   return $result;
 }
-
-//https://qiita.com/Hiraku/items/71873bf31e503eb1b4e1
-
-
-
-/*
-create temporary table tmp as (select ncode,  similarity from Naro_similarity_201 where similarity <  0.4015710949897766 limit 60) union (select ncode,  similarity from Naro_All_info where similarity >  0.4015710949897766 limit 60) order by abs(similarity -  0.4015710949897766) limit 20 offset 0;
-alter table tmp add id int not null primary key auto_increment first;
-select tmp.ncode, Naro_All_info.title from tmp inner join Naro_All_info on tmp.ncode = Naro_All_info.ncode order by tmp.id limit 10 offset 0;
-
-select ncode, writer, title, story from Naro_All_info where ncode = any(select ncode from tmp limit 10 offset 0);
-
-
-select tmp2.ncode, Naro_All_info.title from tmp2 inner join Naro_All_info on tmp2.ncode = Naro_All_info.ncode order by tmp2.id;*/
-
 ?>
